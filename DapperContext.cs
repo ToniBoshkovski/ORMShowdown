55ac3da111dc9fe388cb640bff849a3f357a41fb
Toni Boshkovski <toni.boshkovski@polarcape.com>
2023-06-23 01:33:18 +0200

    Added dbContext

---
+++ DapperContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ORMShowdown
+{
+    public class DapperContext
+    {
+        public IDbConnection CreateConnection() => new SqlConnection("Server=.; Initial Catalog=ORMShowdown; MultipleActiveResultSets=true; Integrated Security=true;");
+    }
+}
---
+++ EFCoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ORMShowdown
+{
+    public class EFCoreDbContext : DbContext
+    {
+        public EFCoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Initial Catalog=ORMShowdown; MultipleActiveResultSets=true; Integrated Security=true;");
+        }
+    }
+}
